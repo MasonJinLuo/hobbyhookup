80ece2ff6d000bfb0d520b38de2e901705a49f9c
Stefanos Bezanis <stefbezanis@gmail.com>
2017-07-20 18:57:10 -0500

    updated homepage with nav bar

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -11,6 +11,43 @@ var Homepage = React.createClass({
 		return (
 					<div id="wrapper">
 
+					<div className="container navContainer">
+            		<nav className="navbar navbar-default">
+            		  <div className="container-fluid">
+            		    
+            		    <div className="navbar-header">
+            		      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            		        <span className="sr-only">Toggle navigation</span>
+            		        <span className="icon-bar"></span>
+            		        <span className="icon-bar"></span>
+            		        <span className="icon-bar"></span>
+            		      </button>
+            		      <h3 className="navbar-brand" href="/home">Hobby Hookup</h3>
+            		    </div>
+		
+            		      <form className="navbar-form navbar-left">
+            		        <div className="form-group">
+            		          <input type="text" className="form-control" placeholder="Search"></input>
+            		        </div>
+            		        <button type="submit" className="btn btn-default">Submit</button>
+            		      </form>
+            		      <div className="nav navbar-nav navbar-right">
+            		        <div className="dropdown">
+            		            <button className="dropbtn btn btn-lg btn-default"> 
+            		                <span className="glyphicon glyphicon-list"></span> 
+            		            </button>
+            		            <div className="dropdown-content">
+            		                <a href="#">Profile</a>
+            		                <a href="#">Inbox</a>
+            		                <a href="#">Update</a>
+            		                <a href="#">Logout</a>
+            		            </div>
+            		        </div>
+            		      </div>
+            		    </div>
+            		</nav>    
+       			</div>
+
 					<Intro/>
 
 				
@@ -23,7 +60,7 @@ var Homepage = React.createClass({
 					<nav id="nav">
 						<ul className="links">
 							<li className="active"><a href="index.html">Hobby Hookup</a></li>
-							<li><a href="about.html">About Us</a></li>
+							<Link to="/Profile"><li><a href="profile.html">About Us</a></li></Link>
 						</ul>
 						<ul className="icons">
 							<li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
